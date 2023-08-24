819d018ec03020b3685e5e34ea007e04f31eaf5a
Maria Antonia <mdeazevedomaia@gmail.com>
2023-08-24 12:19:51 -0300

    Refactoring unittest methods

--- tests/test_cpf.py
+++ tests/test_cpf.py
@@ -25,12 +25,14 @@ from unittest import TestCase, main
 
 class CPF(TestCase):
     def test_sieve(self):
-        assert sieve("00000000000") == "00000000000"
-        assert sieve("123.456.789-10") == "12345678910"
-        assert sieve("134..2435.-1892.-") == "13424351892"
-        assert sieve("abc1230916*!*&#") == "abc1230916*!*&#"
-        assert sieve("ab.c1.--.2-309.-1-.6-.*.-!*&#") == "abc1230916*!*&#"
-        assert sieve("...---...") == ""
+        self.assertEqual(sieve("00000000000"), "00000000000")
+        self.assertEqual(sieve("123.456.789-10"), "12345678910")
+        self.assertEqual(sieve("134..2435.-1892.-"), "13424351892")
+        self.assertEqual(sieve("abc1230916*!*&#"), "abc1230916*!*&#")
+        self.assertEqual(
+            sieve("ab.c1.--.2-309.-1-.6-.*.-!*&#"), "abc1230916*!*&#"
+        )
+        self.assertEqual(sieve("...---..."), "")
 
     def test_remove_symbols(self):
         with patch("brutils.cpf.sieve") as mock_sieve:
@@ -39,71 +41,71 @@ class CPF(TestCase):
             mock_sieve.assert_called()
 
     def test_display(self):
-        assert display("00000000011") == "000.000.000-11"
-        assert display("00000000000") is None
-        assert display("0000000000a") is None
-        assert display("000000000000") is None
+        self.assertEqual(display("00000000011"), "000.000.000-11")
+        self.assertIsNone(display("00000000000"))
+        self.assertIsNone(display("0000000000a"))
+        self.assertIsNone(display("000000000000"))
 
     def test_format_cpf(self):
         with patch("brutils.cpf.is_valid", return_value=True) as mock_is_valid:
             # When cpf is_valid, returns formatted cpf
-            assert format_cpf("11144477735") == "111.444.777-35"
+            self.assertEqual(format_cpf("11144477735"), "111.444.777-35")
 
         # Checks if function is_valid_cpf is called
         mock_is_valid.assert_called_once_with("11144477735")
 
         with patch("brutils.cpf.is_valid", return_value=False) as mock_is_valid:
             # When cpf isn't valid, returns None
-            assert format_cpf("11144477735") is None
+            self.assertIsNone(format_cpf("11144477735"))
 
     def test_validate(self):
-        assert validate("52513127765")
-        assert validate("52599927765")
-        assert not validate("00000000000")
+        self.assertTrue(validate("52513127765"))
+        self.assertTrue(validate("52599927765"))
+        self.assertFalse(validate("00000000000"))
 
     def test_is_valid(self):
         # When cpf is not string, returns False
-        assert not is_valid(1)
+        self.assertFalse(is_valid(1))
 
         # When cpf's len is different of 11, returns False
-        assert not is_valid("1")
+        self.assertFalse(is_valid("1"))
 
         # When cpf does not contain only digits, returns False
-        assert not is_valid("1112223334-")
+        self.assertFalse(is_valid("1112223334-"))
 
         # When CPF has only the same digit, returns false
-        assert not is_valid("11111111111")
+        self.assertFalse(is_valid("11111111111"))
 
         # When rest_1 is lt 2 and the 10th digit is not 0, returns False
-        assert not is_valid("11111111215")
+        self.assertFalse(is_valid("11111111215"))
 
         # When rest_1 is gte 2 and the 10th digit is not (11 - rest), returns False
-        assert not is_valid("11144477705")
+        self.assertFalse(is_valid("11144477705"))
 
         # When rest_2 is lt 2 and the 11th digit is not 0, returns False
-        assert not is_valid("11111111204")
+        self.assertFalse(is_valid("11111111204"))
 
         # When rest_2 is gte 2 and the 11th digit is not (11 - rest), returns False
-        assert not is_valid("11144477732")
+        self.assertFalse(is_valid("11144477732"))
 
         # When cpf is valid
-        assert is_valid("11144477735")
-        assert is_valid("11111111200")
+        self.assertTrue(is_valid("11144477735"))
+        self.assertTrue(is_valid("11111111200"))
 
     def test_generate(self):
         for _ in range(10_000):
-            assert validate(generate())
-            assert display(generate()) is not None
+            self.assertTrue(validate(generate()))
+            self.assertIsNotNone(display(generate()))
 
     def test__hashdigit(self):
-        assert _hashdigit("000000000", 10) == 0
-        assert _hashdigit("0000000000", 11) == 0
-        assert _hashdigit("52513127765", 10) == 6
-        assert _hashdigit("52513127765", 11) == 5
+        self.assertEqual(_hashdigit("000000000", 10), 0)
+        self.assertEqual(_hashdigit("0000000000", 11), 0)
+        self.assertEqual(_hashdigit("52513127765", 10), 6)
+        self.assertEqual(_hashdigit("52513127765", 11), 5)
 
     def test_checksum(self):
-        assert _checksum("000000000") == "00"
-        assert _checksum("525131277") == "65"
+        self.assertEqual(_checksum("000000000"), "00")
+        self.assertEqual(_checksum("525131277"), "65")
 
 
 if __name__ == "__main__":
