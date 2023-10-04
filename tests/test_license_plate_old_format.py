2f8fd21306407179eb1793aac16816bf894f4220
Hebert do Nascimento <hebertn88@gmail.com>
2023-10-04 12:33:48 -0300

    Correção da formatação (Lint)

--- tests/test_license_plate_old_format.py
+++ tests/test_license_plate_old_format.py
@@ -1,6 +1,7 @@
 from unittest import TestCase, main
 from brutils.license_plate import is_valid_license_plate_old_format
 
+
 class TestLicenSePlate(TestCase):
     def test_is_valid_license_old_format(self):
         # When license plate is valid, returns True
@@ -9,15 +10,14 @@ class TestLicenSePlate(TestCase):
 
         # When license plate is valid with whitespaces, returns True
         self.assertTrue(is_valid_license_plate_old_format(" ABC123    "))
-        
+
         # When license plate is not string, returns False
         self.assertFalse(is_valid_license_plate_old_format(123456))
 
         # When license plate is invalid with special characters, returns False
         self.assertFalse(is_valid_license_plate_old_format("ABC-123"))
 
-        # When license plate is invalid with numbers and letters out of order,
-        # returns False
+        # When license plate is invalid with numbers and letters out of order, returns False
         self.assertFalse(is_valid_license_plate_old_format("A1CA23"))
 
         # When license plate is invalid with new format, returns False
