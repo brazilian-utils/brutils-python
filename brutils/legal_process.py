4695d5c1d6851804312da2dc4a493e153be424f6
Marcel Cattaneo <marcel.cattaneo@deal.com.br>
2023-10-05 10:24:07 -0300

    adjusting legal_process logics

--- brutils/legal_process.py
+++ brutils/legal_process.py
@@ -4,7 +4,7 @@ import re
 ############
 
 
-def sieve(dirty):  # type: (str) -> list
+def sieve(dirty):  # type: (str) -> str
     """
     Filters out Legal Process ID number formatting symbols.
 
@@ -13,7 +13,7 @@ def sieve(dirty):  # type: (str) -> list
     because their presence indicate that the input was somehow corrupted.
     """
 
-    return re.findall(r"[^\w\s\-.]", dirty)
+    return re.sub(r"[-.]", "", dirty)
 
 
 def remove_symbols(dirty):  # type: (str) -> str
@@ -29,20 +29,20 @@ def display(legal_process_id):
 
     if (
         not legal_process_id.isdigit()
-        or len(legal_process_id) != 11
+        or len(legal_process_id) != 20
         or len(set(legal_process_id)) == 1
     ):
         return None
     return format_processo_juridico(legal_process_id)
 
 
-def format_processo_juridico(legal_process_id):  # type: (int) -> (str)
+def format_processo_juridico(legal_process_id):  # type: (str) -> (str)
     """
     Format an adequately formatted numbers-only Legal Process ID number,
     Returns a Legal Process ID number formatted with standard visual aid symbols.
     Returns None if Legal Process ID number is invalid.
     """
-    if isinstance(legal_process_id, int):
+    if legal_process_id.isdigit() and len(legal_process_id) == 20:
         capture_fields = r"(\d{7})(\d{2})(\d{4})(\d)(\d{2})(\d{4})"
         include_chars = r"\1-\2.\3.\4.\5.\6"
         return re.sub(capture_fields, include_chars, legal_process_id)
