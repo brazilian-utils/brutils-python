1bb9a0de9b76238968aaecc487cd9d23d73e197c
Jaime Nunes Leal <jaimenunesleal@gmail.com>
2024-10-15 22:50:32 -0300

    Update Format

--- tests/ibge/test_municipality.py
+++ tests/ibge/test_municipality.py
@@ -33,13 +33,12 @@ class TestIBGE(TestCase):
         )
         result = get_municipality_by_code("342432")
         self.assertIsNone(result)
-    
+
     @patch("brutils.ibge.municipality.urlopen")
     def test_get_municipality_excpetion(self, mock):
-        mock.side_effect = Exception('Erro desconhecido')
+        mock.side_effect = Exception("Erro desconhecido")
         result = get_municipality_by_code("342432")
         self.assertIsNone(result)
-
 
     @patch("brutils.ibge.municipality.urlopen")
     def test_successfull_decompression(self, mock_urlopen):
@@ -47,9 +46,7 @@ class TestIBGE(TestCase):
         compressed_data = gzip.compress(valid_json.encode("utf-8"))
         mock_response = MagicMock()
         mock_response.read.return_value = compressed_data
-        mock_response.info.return_value.get.return_value = (
-            "gzip" 
-        )
+        mock_response.info.return_value.get.return_value = "gzip"
         mock_urlopen.return_value.__enter__.return_value = mock_response
 
         result = get_municipality_by_code("3550308")
@@ -67,23 +64,27 @@ class TestIBGE(TestCase):
 
     @patch("gzip.GzipFile.read", side_effect=OSError("Erro na descompressão"))
     def test_error_decompression(self, mock_gzip_read):
-        result =  get_municipality_by_code("3550308")
+        result = get_municipality_by_code("3550308")
         self.assertIsNone(result)
 
-    @patch("gzip.GzipFile.read", side_effect=Exception("Erro desconhecido na descompressão"))
+    @patch(
+        "gzip.GzipFile.read",
+        side_effect=Exception("Erro desconhecido na descompressão"),
+    )
     def test_error_decompression_generic_exception(self, mock_gzip_read):
-        result =  get_municipality_by_code("3550308")
+        result = get_municipality_by_code("3550308")
         self.assertIsNone(result)
 
-    @patch("json.loads", side_effect=JSONDecodeError("error", 'city.json', 1))
+    @patch("json.loads", side_effect=JSONDecodeError("error", "city.json", 1))
     def test_error_json_load(self, mock_json_loads):
-        result =  get_municipality_by_code("3550308")
+        result = get_municipality_by_code("3550308")
         self.assertIsNone(result)
-    
+
     @patch("json.loads", side_effect=KeyError)
     def test_error_json_key_error(self, mock_json_loads):
-        result =  get_municipality_by_code("3550308")
+        result = get_municipality_by_code("3550308")
         self.assertIsNone(result)
+
 
 if __name__ == "__main__":
     main()
