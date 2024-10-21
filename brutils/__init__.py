a2d8a7d8899234c2db1f4aeccd0ae755a5289b44
Willian Morais <williandmorais@gmail.com>
2024-10-21 15:27:06 -0300

    feat: adiciona formatação monetária

--- brutils/__init__.py
+++ brutils/__init__.py
@@ -4,85 +4,47 @@ from brutils.cep import (
     get_address_from_cep,
     get_cep_information_from_address,
 )
-from brutils.cep import (
-    generate as generate_cep,
-)
-from brutils.cep import (
-    is_valid as is_valid_cep,
-)
-from brutils.cep import (
-    remove_symbols as remove_symbols_cep,
-)
+from brutils.cep import generate as generate_cep
+from brutils.cep import is_valid as is_valid_cep
+from brutils.cep import remove_symbols as remove_symbols_cep
 
 # CNPJ Imports
-from brutils.cnpj import (
-    format_cnpj,
-)
-from brutils.cnpj import (
-    generate as generate_cnpj,
-)
-from brutils.cnpj import (
-    is_valid as is_valid_cnpj,
-)
-from brutils.cnpj import (
-    remove_symbols as remove_symbols_cnpj,
-)
+from brutils.cnpj import format_cnpj
+from brutils.cnpj import generate as generate_cnpj
+from brutils.cnpj import is_valid as is_valid_cnpj
+from brutils.cnpj import remove_symbols as remove_symbols_cnpj
 
 # CPF Imports
-from brutils.cpf import (
-    format_cpf,
-)
-from brutils.cpf import (
-    generate as generate_cpf,
-)
-from brutils.cpf import (
-    is_valid as is_valid_cpf,
-)
-from brutils.cpf import (
-    remove_symbols as remove_symbols_cpf,
-)
+from brutils.cpf import format_cpf
+from brutils.cpf import generate as generate_cpf
+from brutils.cpf import is_valid as is_valid_cpf
+from brutils.cpf import remove_symbols as remove_symbols_cpf
+
+# Currency
+# Currency
+from brutils.currency import format_currency
 
 # Email Import
 from brutils.email import is_valid as is_valid_email
 
 # IBGE Imports
-from brutils.ibge.uf import (
-    convert_code_to_uf,
-)
+from brutils.ibge.uf import convert_code_to_uf
 
 # Legal Process Imports
-from brutils.legal_process import (
-    format_legal_process,
-)
-from brutils.legal_process import (
-    generate as generate_legal_process,
-)
-from brutils.legal_process import (
-    is_valid as is_valid_legal_process,
-)
-from brutils.legal_process import (
-    remove_symbols as remove_symbols_legal_process,
-)
+from brutils.legal_process import format_legal_process
+from brutils.legal_process import generate as generate_legal_process
+from brutils.legal_process import is_valid as is_valid_legal_process
+from brutils.legal_process import remove_symbols as remove_symbols_legal_process
 
 # License Plate Imports
 from brutils.license_plate import (
     convert_to_mercosul as convert_license_plate_to_mercosul,
 )
-from brutils.license_plate import (
-    format_license_plate,
-)
-from brutils.license_plate import (
-    generate as generate_license_plate,
-)
-from brutils.license_plate import (
-    get_format as get_format_license_plate,
-)
-from brutils.license_plate import (
-    is_valid as is_valid_license_plate,
-)
-from brutils.license_plate import (
-    remove_symbols as remove_symbols_license_plate,
-)
+from brutils.license_plate import format_license_plate
+from brutils.license_plate import generate as generate_license_plate
+from brutils.license_plate import get_format as get_format_license_plate
+from brutils.license_plate import is_valid as is_valid_license_plate
+from brutils.license_plate import remove_symbols as remove_symbols_license_plate
 
 # Phone Imports
 from brutils.phone import (
@@ -90,37 +52,19 @@ from brutils.phone import (
     remove_international_dialing_code,
     remove_symbols_phone,
 )
-from brutils.phone import (
-    generate as generate_phone,
-)
-from brutils.phone import (
-    is_valid as is_valid_phone,
-)
+from brutils.phone import generate as generate_phone
+from brutils.phone import is_valid as is_valid_phone
 
 # PIS Imports
-from brutils.pis import (
-    format_pis,
-)
-from brutils.pis import (
-    generate as generate_pis,
-)
-from brutils.pis import (
-    is_valid as is_valid_pis,
-)
-from brutils.pis import (
-    remove_symbols as remove_symbols_pis,
-)
+from brutils.pis import format_pis
+from brutils.pis import generate as generate_pis
+from brutils.pis import is_valid as is_valid_pis
+from brutils.pis import remove_symbols as remove_symbols_pis
 
 # Voter ID Imports
-from brutils.voter_id import (
-    format_voter_id,
-)
-from brutils.voter_id import (
-    generate as generate_voter_id,
-)
-from brutils.voter_id import (
-    is_valid as is_valid_voter_id,
-)
+from brutils.voter_id import format_voter_id
+from brutils.voter_id import generate as generate_voter_id
+from brutils.voter_id import is_valid as is_valid_voter_id
 
 # Defining __all__ to expose the public methods
 __all__ = [
@@ -172,4 +116,6 @@ __all__ = [
     "is_valid_voter_id",
     # IBGE
     "convert_code_to_uf",
+    # Currency
+    "format_currency",
 ]
