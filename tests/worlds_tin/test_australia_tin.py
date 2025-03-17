d96de8deeb6cda837222c0172a8c183946dace2b
Carol Cardoso <carolcardoso080@gmail.com>
2025-03-17 17:38:15 -0300

    atualização do código de teste

--- tests/worlds_tin/test_australia_tin.py
+++ tests/worlds_tin/test_australia_tin.py
@@ -2,13 +2,12 @@ from unittest import TestCase, main
 from unittest.mock import patch
 
 from tin_australia import (
-    remove_symbols,
-    is_valid_tfn,
-    is_valid_abn,
     format_tfn,
-    format_abn,
-    generate_tfn,
     generate_abn,
+    generate_tfn,
+    is_valid_abn,
+    is_valid_tfn,
+    remove_symbols,
 )
 
 
@@ -20,13 +19,24 @@ class TestTFNABN(TestCase):
 
     def test_is_valid_tfn(self):
         self.assertEqual(is_valid_tfn("123456782"), "Valid TFN")
-        self.assertEqual(is_valid_tfn("12345678"), "Invalid TFN: Failed checksum validation.")
-        self.assertEqual(is_valid_tfn("A23456789"), "Invalid TFN: Must be 8 or 9 numeric digits.")
+        self.assertEqual(
+            is_valid_tfn("12345678"), "Invalid TFN: Failed checksum validation."
+        )
+        self.assertEqual(
+            is_valid_tfn("A23456789"),
+            "Invalid TFN: Must be 8 or 9 numeric digits.",
+        )
 
     def test_is_valid_abn(self):
         self.assertEqual(is_valid_abn("51824753556"), "Valid ABN")
-        self.assertEqual(is_valid_abn("12345678901"), "Invalid ABN: Failed checksum validation.")
-        self.assertEqual(is_valid_abn("A2345678901"), "Invalid ABN: Must be exactly 11 numeric digits.")
+        self.assertEqual(
+            is_valid_abn("12345678901"),
+            "Invalid ABN: Failed checksum validation.",
+        )
+        self.assertEqual(
+            is_valid_abn("A2345678901"),
+            "Invalid ABN: Must be exactly 11 numeric digits.",
+        )
 
     def test_generate_tfn(self):
         for _ in range(100):
@@ -42,6 +52,7 @@ class TestTFNABN(TestCase):
     def test_format_tfn(self, mock_is_valid_tfn):
         mock_is_valid_tfn.return_value = "Valid TFN"
         self.assertEqual(format_tfn("123456782"), "12 345 6782")
+
     def test_format_tin(self, mock_is_valid):
         mock_is_valid.return_value = "Valid Anguilla TIN"
         self.assertEqual(format_tin("AB12345678"), "AB-12345678")
